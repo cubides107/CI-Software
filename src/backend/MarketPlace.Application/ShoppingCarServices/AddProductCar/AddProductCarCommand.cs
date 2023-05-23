7c1a05e3e65e3a1ef881f4803bbfba0bc5e99227
Davison Castelblanco <davisonju26@gmail.com>
2023-05-22 21:22:18 -0500

    retornar la lista de productos agregados al carrito

--- src/backend/MarketPlace.Application/ShoppingCarServices/AddProductCar/AddProductCarCommand.cs
+++ src/backend/MarketPlace.Application/ShoppingCarServices/AddProductCar/AddProductCarCommand.cs
@@ -1,10 +1,11 @@
 using System.Security.Claims;
+using CreditApp.Application.ProductServices.GetProduct;
 using CreditApp.Application.ShoppingCarServices.GetShoppingCar;
 using MediatR;
 
 namespace CreditApp.Application.ShoppingCarServices.AddProductCar;
 
-public class AddProductCarCommand : IRequest<int>
+public class AddProductCarCommand : IRequest<int>, IRequest<List<GetProductDTO>>
 {
     public List<Claim> UserClaims { get; set; }
     public List<int> ProducId { get; set; }
--- src/backend/MarketPlace.Application/ShoppingCarServices/AddProductCar/AddProductCarHandler.cs
+++ src/backend/MarketPlace.Application/ShoppingCarServices/AddProductCar/AddProductCarHandler.cs
@@ -1,3 +1,4 @@
+using CreditApp.Application.ProductServices.GetProduct;
 using CreditApp.Application.ShoppingCarServices.GetShoppingCar;
 using MarketPlace.Domain;
 using MarketPlace.Domain.ProductEntities;
@@ -7,34 +8,42 @@ using MediatR;
 
 namespace CreditApp.Application.ShoppingCarServices.AddProductCar;
 
-public class AddProductCarHandler : IRequestHandler<AddProductCarCommand, int>
+public class AddProductCarHandler : IRequestHandler<AddProductCarCommand, List<GetProductDTO>>
 {
 
     private readonly IRepository Repository;
+    private readonly IMapObject MapObject; 
     private readonly ISecurity Security;
 
-    public AddProductCarHandler(IRepository repository, ISecurity security)
+    public AddProductCarHandler(IRepository repository, IMapObject mapObject, ISecurity security)
     {
         Repository = repository;
+        MapObject = mapObject;
         Security = security;
     }
     
-    public async Task<int> Handle(AddProductCarCommand request, CancellationToken cancellationToken)
+    public async Task<List<GetProductDTO>> Handle(AddProductCarCommand request, CancellationToken cancellationToken)
     {
         int userId = int.Parse(Security.GetClaim(request.UserClaims, claimType: ISecurity.USERID));
         List<int> productsId = request.ProducId;
         ShoppingCar shoppingCar = new ShoppingCar(userId);
+        List<GetProductDTO> productDtos = new List<GetProductDTO>();
         foreach (int productId in productsId)
         {
-        bool exist = Repository.Exists<Product>(product1 => product1.Id == productId);
-        Product product = null;
-        if (exist) product = await Repository.Get<Product>(product2 => product2.Id == productId);
-        if (product != null) product.AddShoppingCar(shoppingCar);
+            bool exist = Repository.Exists<Product>(product1 => product1.Id == productId);
+            Product product = null;
+            if (exist)
+            {
+                product = await Repository.Get<Product>(product2 => product2.Id == productId);
+                productDtos.Add(MapObject.Map<Product, GetProductDTO>(product));
+            }
+            
+            if (product != null) product.AddShoppingCar(shoppingCar);
         }
         
         await Repository.Save(shoppingCar);
         await Repository.Commit();
 
-        return 0;
+        return productDtos;
     }
 }
