fee19271c817cd598a01da854cc5d28761535883
Davison Castelblanco <davisonju26@gmail.com>
2023-05-23 21:43:16 -0500

    se exponen los ids de los productos y al agregar al carrito se envian los productos y el total

--- src/backend/MarketPlace.Application/ShoppingCarServices/GetShoppingCar/GetShoppingCarDTO.cs
+++ src/backend/MarketPlace.Application/ShoppingCarServices/GetShoppingCar/GetShoppingCarDTO.cs
@@ -1,8 +1,9 @@
-using MarketPlace.Domain.ProductEntities;
+using CreditApp.Application.ProductServices.GetProduct;
 
 namespace CreditApp.Application.ShoppingCarServices.GetShoppingCar;
 
 public class GetShoppingCarDTO 
 {
-    public int UserId { get; set; }
+    public List<GetProductDTO> Products { get; set; }
+    public double TotalPrice { get; set; }
 }
